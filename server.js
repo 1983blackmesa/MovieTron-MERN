ba839bc347cf70bd3535f224804528feb351f22d
1983blackmesa <Masis23@gmail.com>
2022-05-07 14:40:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const fileUpload = require('express-fileupload')
-const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
+const app = express(); //create express app
+//middleware functions
+app.use(express.json()); //pass JSON request
+app.use(cors());
+app.use(cookieParser());
 app.use(fileUpload({
     useTempFiles: true
 }))
 
-// Routes
-app.use('/user', require('./routes/userRouter'))
-app.use('/api', require('./routes/upload')), 
-app.use('/api', require('./routes/movie-router'))
+// Middleware Routes
+app.use('/user', require('./routes/userRouter'));
+app.use('/api', require('./routes/upload'));
+app.use('/api', require('./routes/movie-router'));
 
 
 
 // Connect to mongodb
-const URI = process.env.MONGODB_URL
+const URI = process.env.MONGODB_URL; //pass to .env file
 mongoose.connect(URI, {
     //useCreateIndex: true,
     //useFindAndModify: false,
@@ -31,10 +32,11 @@ mongoose.connect(URI, {
     useUnifiedTopology: true
 }, err => {
     if(err) throw err;
-    console.log("Connected to mongodb")
+    console.log("Connected to mongodb"); //connected to mongo cloudb
 })
 
-/*
+/* 
+//For Production use when site goes live 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
     app.get('*', (req, res)=>{
@@ -45,5 +47,5 @@ if(process.env.NODE_ENV === 'production'){
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT)
+    console.log('Server is running on port', PORT) //listen port 5000 in the backend
 })
